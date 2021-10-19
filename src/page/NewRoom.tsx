4dd5026037ec61190202f4b2367103e25d773a59
Victor Rosa <victorsouzarosa100@gmail.com>
2021-10-19 12:12:11 -0300

    Initial Commit

---
+++ src/page/NewRoom.tsx
@@ -0,0 +1,47 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../App';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+
+
+import '../styles/auth.scss';
+
+
+
+export function NewRoom(){
+  const {user} = useContext(AuthContext)
+
+  return (
+    <div id="page-auth" >
+      <aside>
+        <img src={illustrationImg} alt=" ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className='main-content' >
+          <img src={logoImg} alt="letmeask"/>
+          <h2>Criar uma nova Sala</h2>
+          <h1>{user?.name}</h1>
+          <div className='separator'>ou entre em um sala</div>
+          <form >
+            <input 
+            type="text"
+            placeholder="Nome da sala" 
+            />
+            <Button  type="submit">
+              Criar Sala
+            </Button>
+          </form>
+            <p>
+              Quer entrar em um sala já existente? <Link to="/">Clique aqui</Link>
+            </p>
+        </div>
+      </main>
+    </div>
+  )
+}  
